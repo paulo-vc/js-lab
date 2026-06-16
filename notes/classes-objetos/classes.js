7eff377c4fbf9afee28c00dad26eb77678656398
paulo-vc <contato.paulovruffini@gmail.com>
2026-06-16 11:09:30 -0300

    finalizando modulo classes e objetos

--- notes/classes-objetos/classes.js
+++ notes/classes-objetos/classes.js
@@ -1,6 +1,15 @@
 class Pessoa {
     nome;
     idade;
+    anoDeNascimento;
+
+    //construtor
+
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2026 - idade;
+    }
 
     // métodos não precisam ter a palavra function
     descrever(){
@@ -8,11 +17,29 @@ class Pessoa {
     }
 }
 
-const paulo = new Pessoa();
+function compararPessoas(p1, p2){
+    if(p1.idade > p2.idade){
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    }else if(p2.idade > p1.idade){
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    }else{
+        console.log(`${p1.nome} tem a mesma idade que ${p2.nome}`);
+    }
+}
+
+const dev = {nome: 'Paulo', linguagemPreferida: 'JavaScript'};
+console.log(dev.linguagemPreferida);
+
+const paulo = new Pessoa('Paulo', 23);
+const borges = new Pessoa('Borges', 21);
+
+compararPessoas(paulo, borges);
+
+new Pessoa('Renan', 30).descrever();
 
 console.log(paulo);
 
 paulo.nome = "Paulo Vinicius";
-paulo.idade = 22;
+paulo.idade = 23;
 
 console.log(paulo);
